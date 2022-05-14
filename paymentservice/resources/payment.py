91bcc26b1e56dc012effdcff0795c7c2716ab485
TJKNL <39524446+TJKNL@users.noreply.github.com>
2022-05-14 21:55:50 +0200

    Update payment.py

--- paymentservice/resources/payment.py
+++ paymentservice/resources/payment.py
@@ -6,7 +6,7 @@ class Payment:
     @staticmethod
     def bank_request(amount):
         # Generate a random number to simulate failed payments. This functions simulates the bank.
-        if random.randint(0, 100) < 95:
+        if random.randint(0, 100) < 50:
             return jsonify({"output": f"payment of {amount} successful"}), 200
         else:
             return jsonify({"output": f"payment of {amount} was not successful"}), 500
