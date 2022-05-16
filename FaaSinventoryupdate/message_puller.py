d30888b7fa7a0622b6d6bd7f907f357e47af202d
SemUijen <grootveer.sem@outlook.com>
2022-05-16 20:27:19 +0200

    UpdateMessagePuller

--- FaaSinventoryupdate/message_puller.py
+++ FaaSinventoryupdate/message_puller.py
@@ -10,7 +10,6 @@ from pub_sub_util import publish_message
 import sys
 
 def pull_message(project, subscription, order):
-    print("Test2")
     subscriber = pubsub_v1.SubscriberClient()
 
     subscription_path = subscriber.subscription_path(project, subscription)
@@ -22,9 +21,9 @@ def pull_message(project, subscription, order):
         data = json.loads(message.data.decode("utf-8"))
 
         logging.info("The new_order is received")
-        order.create(data)
-        unfulfilled_orders = order.get_unfulfilled()
-        publish_message(project=project, topic="unfulfilled_orders", message=unfulfilled_orders, event_type="NewOrderAdded")
+        #order.create(data)
+        #unfulfilled_orders = order.get_unfulfilled()
+        #publish_message(project=project, topic="unfulfilled_orders", message=unfulfilled_orders, event_type="NewOrderAdded")
         message.ack()
 
     streaming_pull_future = subscriber.subscribe(
@@ -49,71 +48,20 @@ class MessagePuller(Thread):
         Thread.__init__(self)
         self.project_id = project
         self.subscription_id = subscription
-        self.daemon = True
+        #self.daemon = True
         self.orders = orders
         self.start()
 
     def run(self):
         while True:
             try:
-                print("Test1")
                 pull_message(self.project_id, self.subscription_id, self.orders)
                 time.sleep(30)
-            except:
-                print("Error:", sys.exc_info()[0])
 
-
-
-"""def pull_message2(project, subscription, order):
-    subscriber = pubsub_v1.SubscriberClient()
-    subscription_path = subscriber.subscription_path(project, subscription)
-
-    def callback2(message):
-
-        logging.info(f"Received {message.data}.")
-        # event_type = message.attributes.get("event_type")   # event type as a message attribute
-        data = json.loads(message.data.decode("utf-8"))
-
-        logging.info("The order status change is received")
-        order.update_status(data)
-        unfulfilled_orders = order.get_unfulfilled()
-        publish_message(project=project, topic="unfulfilled_orders", message=unfulfilled_orders,
-                        event_type="OrderStatusUpdated")
-        message.ack()
-
-    streaming_pull_future = subscriber.subscribe(
-        subscription_path, callback=callback2, await_callbacks_on_shutdown=True,
-    )
-    logging.info(f"Listening for messages on {subscription_path}..\n")
-
-    # Wrap subscriber in a 'with' block to automatically call close() when done.
-    with subscriber:
-        try:
-            # When `timeout` is not set, result() will block indefinitely,
-            # unless an exception is encountered first.
-            streaming_pull_future.result(timeout=60)
-        except TimeoutError:
-            streaming_pull_future.cancel()
-            logging.info("Streaming pull future canceled.")"""
-
-
-"""class MessagePuller2(Thread):
-    def __init__(self, project, subscription, orders):
-        Thread.__init__(self)
-        self.project_id = project
-        self.subscription_id = subscription
-        self.daemon = True
-        self.orders = orders
-        self.start()
-
-    def run(self):
-
-        while True:
-
-            try:
-                pull_message2(self.project_id, self.subscription_id, self.orders)
-                time.sleep(30)
             except Exception as ex:
                 logging.info(f"Listening for messages on {self.subscription_id} threw an exception: {ex}.")
                 time.sleep(30)
-"""
+
+
+
+
