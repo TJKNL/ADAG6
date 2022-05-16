4a4b01698aaceef0afa9b1fb358bd2c1faa40ebb
ElskeNijhof <elske.nijhof@outlook.com>
2022-05-16 14:40:13 +0200

    Imports toegevoegd

--- fulfillment_ui/app.py
+++ fulfillment_ui/app.py
@@ -1,6 +1,12 @@
-from resources.fulfillmentform import Fullfillmentform
+from resources.fulfillmentform import FulfillmentForm
 
+
+from inventorydirectservice.db import Base, engine
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+Base.metadata.create_all(engine)
 
 @app.route('/fulfillment_ui', methods=['GET', 'POST'])
 def index():
-    return Fullfillmentform.index()
+    return FulfillmentForm.index()
