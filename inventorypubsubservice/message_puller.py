2b0680082dca0bef5f3eedb363b111ab38f26238
SemUijen <grootveer.sem@outlook.com>
2022-05-17 17:31:10 +0200

    Update message_puller.py

--- inventorypubsubservice/message_puller.py
+++ inventorypubsubservice/message_puller.py
@@ -16,7 +16,7 @@ class Callable:
     def callback(self, message):
         logging.info(f"Received {message.data}.")
         data = json.loads(message.data.decode("utf-8"))
-        logging.info('Data:', data)
+        logging.info('Data:', message.data.decode("utf-8"))
         self.inventory.reduce_inventory(order=data)
         message.ack()
 
