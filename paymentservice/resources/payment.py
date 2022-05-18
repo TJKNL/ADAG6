dc4a83c3c9b8c788c395affc049d76f4a28e94aa
SemUijen <grootveer.sem@outlook.com>
2022-05-18 10:49:03 +0200

    Update payment.py

--- paymentservice/resources/payment.py
+++ paymentservice/resources/payment.py
@@ -8,6 +8,7 @@ class Payment:
     @staticmethod
     def bank_request(amount):
         # accept payment if below 30. If above, client lacks te funds to pay for order.
+        amount = int(amount)
         if amount < 30:
             return jsonify({"output": f"payment of {amount} successful"}), 200
         else:
