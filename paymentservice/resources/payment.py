54c2574b06598f6950abfb77d545e9fc19b925fb
SemUijen <grootveer.sem@outlook.com>
2022-05-18 10:32:09 +0200

    Update payment.py

--- paymentservice/resources/payment.py
+++ paymentservice/resources/payment.py
@@ -2,11 +2,13 @@ from flask import jsonify
 import random
 
 
+
+
 class Payment:
     @staticmethod
     def bank_request(amount):
-        # Generate a random number to simulate failed payments. This functions simulates the bank.
-        if random.randint(0, 100) < 50:
+        # accept payment if below 30. If above, client lacks te funds to pay for order.
+        if amount < 30:
             return jsonify({"output": f"payment of {amount} successful"}), 200
         else:
             return jsonify({"output": f"payment of {amount} was not successful"}), 500
