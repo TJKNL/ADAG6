49c4e69b25eb172cb0b8c437a6fdb26d4a3a7b3d
TJKNL <39524446+TJKNL@users.noreply.github.com>
2022-05-18 10:41:53 +0200

    Update ui.py

--- fulfillment_ui/resources/ui.py
+++ fulfillment_ui/resources/ui.py
@@ -23,7 +23,7 @@ class FulfillmentForm(FlaskForm):
 
 def get_unfulfilled():
     r = requests.get(url='http://api_gateway_ct:8081/unfulfilled_order')
-    logging.info(f"Hoi hoi hier is de request {r.data}.")
+    logging.info(f"Hoi hoi hier is de request {r.json}.")
     r2 = json.loads(r.data.decode("utf-8"))
     logging.info(f"Hoi hoi hier is de request {r2}.")
     message = ""
